c357b08bf2894db84fc4841082dc21a7460bb805
lavi <npt1237@gmail.com>
2025-09-09 12:01:36 +0900

    feat: 벤치마크를 위한 구성

---
+++ src/bin/benchmark.rs
@@ -0,0 +1,41 @@
+use std::time::Instant;
+
+fn mandelbrot(width: usize, height: usize, iterations: usize) -> Vec<u32> {
+    let mut pixels: Vec<u32> = Vec::with_capacity(width * height);
+    for _ in 0..(width * height) {
+        pixels.push(0);
+    }
+
+    for y in 0..height {
+        for x in 0..width {
+            let mut zx: f64 = 0.0;
+            let mut zy: f64 = 0.0;
+            let cx: f64 = (x as f64 / width as f64) * 3.5 - 2.5;
+            let cy: f64 = (y as f64 / height as f64) * 2.0 - 1.0;
+            let mut iter: usize = 0;
+
+            while zx * zx + zy * zy < 2.5 && iter < iterations {
+                let tmp: f64 = zx * zx - zy * zy + cx;
+                zy = 2.0 * zx * zy + cy;
+                zx = tmp;
+                iter += 1;
+            }
+
+            pixels[y * width + x] = iter as u32;
+        }
+    }
+
+    return pixels;
+}
+
+fn main() {
+    let iter: usize = 1000;
+    let size: usize = 1024;
+    let start = Instant::now();
+
+    for _ in 0..iter {
+        mandelbrot(size, size, 60);
+    }
+
+    println!("{:?}", start.elapsed() / iter.try_into().unwrap());
+}
